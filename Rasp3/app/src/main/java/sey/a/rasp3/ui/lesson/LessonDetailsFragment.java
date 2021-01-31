1703abac93bb9aa387f2905c471e989c4072b467
A-sey <36642186+a-sey@users.noreply.github.com>
2021-02-01 01:50:22 +0300

    Оформил и заполнил информацию о паре в черновом виде.

--- Rasp3/app/src/main/java/sey/a/rasp3/ui/lesson/LessonDetailsFragment.java
+++ Rasp3/app/src/main/java/sey/a/rasp3/ui/lesson/LessonDetailsFragment.java
@@ -10,8 +10,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
 import sey.a.rasp3.R;
+import sey.a.rasp3.model.Discipline;
+import sey.a.rasp3.model.Lesson;
 import sey.a.rasp3.model.LessonDate;
+import sey.a.rasp3.model.Note;
+import sey.a.rasp3.model.Teacher;
+import sey.a.rasp3.model.Time;
+import sey.a.rasp3.model.Type;
+import sey.a.rasp3.raw.RawLesson;
+import sey.a.rasp3.shell.General;
 
 public class LessonDetailsFragment extends Fragment {
     private View root;
@@ -27,8 +37,34 @@ public class LessonDetailsFragment extends Fragment {
     }
 
     private void fillFields(){
+        Lesson lesson = lessonDate.getLesson();
+        RawLesson rawLesson = General.wet(lesson);
+        Discipline discipline = lesson.getDiscipline();
+        Type type = lesson.getType();
+        Time time = lesson.getTime();
+        List<Teacher> teachers = lesson.getTeachers();
+        List<Note> notes = lessonDate.getNotes();
+
         TextView timeName = root.findViewById(R.id.time_name);
-        timeName.setText(lessonDate.getLesson().getTime().getName());
+        TextView timeStart = root.findViewById(R.id.time_start);
+        TextView timeEnd = root.findViewById(R.id.time_end);
+        TextView typeName = root.findViewById(R.id.type_name);
+        TextView disciplineName = root.findViewById(R.id.discipine_full_name);
+        TextView teacherName = root.findViewById(R.id.teacher_full_name);
+        TextView auditoriumName = root.findViewById(R.id.auditorium);
+
+        timeName.setText(time.getName());
+        timeStart.setText(time.getStartTime().toString());
+        timeEnd.setText(time.getEndTime().toString());
+        typeName.setText(type.getName());
+        disciplineName.setText(discipline.getName());
+        StringBuilder teacherText = new StringBuilder();
+        for(Teacher t: teachers){
+            teacherText.append(t.getName()).append("\n");
+        }
+        teacherName.setText(teacherText.toString().trim());
+        auditoriumName.setText(lesson.getAuditorium());
+
     }
 
     public void setLessonDate(LessonDate lessonDate) {
